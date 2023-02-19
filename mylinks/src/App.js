5abaa326f9876457e9fe1ed55dcc47e2aca06921
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-02-18 21:00:21 -0300

    first commit fr

---
+++ mylinks/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="info">
+          <div className="profile">
+            <div className="piccircle"></div>
+            <h1>@ricardo.rsx</h1>
+            <h2>Editor de vídeo e desenvolvedor front-end.</h2>
+            <hr></hr>
+          </div>          
+        </div>
+      </div>      
+
+      <div class="bgcircle1"></div>
+      <div class="bgcircle2"></div>
+    </div>
+  );
+}
+
+export default App;
